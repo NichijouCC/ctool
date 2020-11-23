c94fc66387d803d1863b758c3bd0b7542f3da431
dongbo <895188445@qq.com>
2020-11-23 13:58:39 +0800

    update(debuffaction):可管理多个副作用

--- src/debuffAction.ts
+++ src/debuffAction.ts
@@ -7,34 +7,67 @@
  * 
  * @example
  * ```typescript
+ * //创建管理副作用实例
  * const ins = DebuffAction.create(() => {
- *     const delay = setTimeout(() => console.log("do something!"), 10000000);
+ *     const delay = setTimeout(() => console.log("do something!"), 1000);
  *     return () => clearTimeout(delay);
  * });
  * 
- * // when need dispose
+ * //执行带副作用方法
+ * ins.executeAnother(() => {
+ *     const delay = setTimeout(() => console.log("do anthor thing!"), 100);
+ *     return () => clearTimeout(delay);
+ * });
+ * 
+ * //添加副作用方法
+ * const a_delay = setTimeout(() => console.log("do something!"), 1000);
+ * ins.appendDebuff(() => {
+ *     console.log("debuff write here");
+ *     clearTimeout(a_delay);
+ * })
+ * 
+ * //执行副作用方法
  * ins.dispose();
  * 
  * ```
  */
 export class DebuffAction {
-    private debuff: Function | undefined;
+    private debuff: Function[] = [];
     /**
      * 副作用释放
      */
     dispose = () => {
-        this.debuff?.();
+        this.debuff.forEach(item => {
+            if (typeof item == "function") {
+                item();
+            }
+        })
+    }
+
+    /**
+     * 执行额外的方法
+     * @param anotherAction 
+     */
+    executeAnother(anotherAction: () => any) {
+        this.debuff.push(anotherAction());
+    }
+    /**
+     * 添加待处理的副作用
+     */
+    appendDebuff(debuff: () => any) {
+        this.debuff.push(debuff);
     }
 
     /**
      * 执行目标方法
      * @param action 目标方法
      */
-    static create(action: () => Function) {
+    static create(action?: () => any) {
         const newAct = new DebuffAction();
         if (action) {
-            newAct.debuff = action();
+            newAct.debuff.push(action());
         }
         return newAct;
     }
 }
+
