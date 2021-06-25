9050392638ffb1870b38642505f2b02884fd4c19
dongbo <895188445@qq.com>
2021-06-25 20:42:35 +0800

    update(obeservableData)：更新"数据对象监听"

--- src/observableData.ts
+++ src/observableData.ts
@@ -1,49 +1,55 @@
 import { EventEmitter } from "./eventEmitter";
 
-interface IprivateEvents {
+interface IPrivateEvents {
     setAtt: { att: string, newValue: any, oldValue: any }
 }
-type IattEvents<T extends object> = {
+type IAttEvents<T extends object> = {
     [key in keyof T]: { newValue: any, oldValue: any }
 }
-export type IproxyEvents<T extends object = {}, K extends object = {}> = IprivateEvents & IattEvents<T> & K;
+export type IProxyEvents<T extends object = {}, K extends object = {}> = IPrivateEvents & IAttEvents<T> & K;
 /**
  * 可监听数据对象
+ * 
+ * @example
+ * ```
+ * //监听target变换
+ * let target={x:2}
+ * let observe=Observable(target);
+ * observe.on("x",(ev)=>{
+ *     console.log(ev.newValue,ev.oldValue);
+ * })
+ * 
+ * observe.x=23;
+ * ```
  */
-export class ObservableData<T extends object = {}, K extends object = {}> extends EventEmitter<IproxyEvents<T, K>> {
-    private _target: this;
-    constructor() { 
+export class ObservableData<T extends object = {}, K extends object = {}> extends EventEmitter<IProxyEvents<T, K>> {
+    private _target: object;
+    private constructor(target:object) { 
         super();
-        this._target=new Proxy<any>(this, {
-            set: function (obj, prop: string, value: any) {
-                let oldValue=(obj as any)[prop];
-                (obj as any)[prop] = value;
-                this.emit("setAtt", { att: prop, newValue: value, oldValue});
-                this.emit(prop, { newValue: value, oldValue });
-                return true;
-            }
-        });
+        this._target=target;
     }
     /**
      * 创建可监听对象，可设置对象自定义的事件类型
      * @param data 
      */
     static create<T extends object, K extends object={}>(data: T): T & ObservableData<T, K> {
-        let container = new ObservableData<T>();
-        Object.keys(data).forEach(item => {
-            if ((data as any)[item] != null) {
-                (container as any)[item] = (data as any)[item];
-            }
-        });
+        let container = new ObservableData<T>(data);
         let proxy = new Proxy(container, {
             set: function (obj, prop: string, value: any) {
-                let oldValue=(obj as any)[prop];
-                (obj as any)[prop] = value;
-                container.emit("setAtt", { att: prop, newValue: value, oldValue});
-                (container as any).emit(prop, { newValue: value, oldValue });
-                return true;
+                if(obj._target){
+                    let oldValue=(obj._target as any)[prop];
+                    (obj._target as any)[prop] = value;
+                    container.emit("setAtt", { att: prop, newValue: value, oldValue});
+                    (container as any).emit(prop, { newValue: value, oldValue });
+                    return true;
+                }
+
             }
         });
         return proxy as any;
     }
+}
+
+export function Observable<T extends object>(target:T){
+    return ObservableData.create(target);
 }
